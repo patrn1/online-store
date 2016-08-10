7a1b7924a6c7741c5730b9c6aa9e627f7e2d0bb9
v-samoylov <samoylov.vladimir@yahoo.com>
2016-08-10 18:30:17 +0500

    upd

--- js/fallback.js
+++ js/fallback.js
@@ -1,9 +1,1 @@
-
-$(".product-card:nth-child(even)").css('margin-left', '60px');
-$(".product-card:nth-child(1)").css('margin-top', '0');
-$(".product-card:nth-child(2)").css('margin-top', '0');
-$(".product-info__article-column:nth-child(1)").css('margin-right', '20px');
-$(".catalogue__link-wrapper:nth-child(2)").css('padding-top', '10px');
-$(".filter:nth-child(even)").css('margin-left', '41px');
-$('.product-card:nth-child(4n - 2) .product-card__cart-btn').css('background-color', '#ff7212;');
-$('.product-card:nth-child(4n - 3) .product-card__cart-btn').css('background-color', '#ff7212;');
+!function(){$(".product-card:nth-child(even)").css("margin-left","60px"),$(".product-card:nth-child(1)").css("margin-top","0"),$(".product-card:nth-child(2)").css("margin-top","0"),$(".product-info__article-column:nth-child(1)").css("margin-right","20px"),$(".catalogue__link-wrapper:nth-child(2)").css("padding-top","10px"),$(".filter:nth-child(even)").css("margin-left","41px"),$(".product-card:nth-child(4n - 2) .product-card__cart-btn").css("background-color","#ff7212;"),$(".product-card:nth-child(4n - 3) .product-card__cart-btn").css("background-color","#ff7212;")}();
